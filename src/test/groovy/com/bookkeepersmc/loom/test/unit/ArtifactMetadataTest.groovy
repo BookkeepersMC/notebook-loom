488de836a67d8adf8b90606a24e64c07768d9b61
CrypticVerse <crypticversemc@gmail.com>
2024-09-16 10:02:02 -0400

    publish stuff, workflow stuff

---
+++ src/test/groovy/com/bookkeepersmc/loom/test/unit/ArtifactMetadataTest.groovy
@@ -0,0 +1,246 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2024 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bookkeepersmc.loom.test.unit
+
+import java.nio.file.Path
+
+import spock.lang.Specification
+
+import net.fabricmc.loom.configuration.mods.ArtifactMetadata
+import net.fabricmc.loom.configuration.mods.ArtifactRef
+
+import static com.bookkeepersmc.loom.test.util.ZipTestUtils.createZip
+import static com.bookkeepersmc.loom.test.util.ZipTestUtils.manifest
+import static net.fabricmc.loom.configuration.mods.ArtifactMetadata.MixinRemapType.MIXIN
+import static net.fabricmc.loom.configuration.mods.ArtifactMetadata.MixinRemapType.STATIC
+import static net.fabricmc.loom.configuration.mods.ArtifactMetadata.RemapRequirements.*
+
+class ArtifactMetadataTest extends Specification {
+	def "is notebook mod"() {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		then:
+		isMod == metadata.isFabricMod()
+		where:
+		isMod | entries
+		false | ["hello.json": "{}"]
+		true | ["notebook.mod.json": "{}"]
+	}
+
+	def "remap requirements"() {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		then:
+		requirements == metadata.remapRequirements()
+		where:
+		requirements | entries
+		DEFAULT      | ["notebook.mod.json": "{}"] 										// Default
+		OPT_OUT      | ["META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "false")] // opt-out
+		OPT_IN       | ["META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "true")]	// opt-in
+	}
+
+	def "Should Remap" () {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		def result = metadata.shouldRemap()
+		then:
+		result == shouldRemap
+		where:
+		shouldRemap | entries
+		false       | ["hello.json": "{}"] 												// None Mod jar
+		true        | ["notebook.mod.json": "{}"] 										// notebook mod
+		false       | ["notebook.mod.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "false")] 	// Fabric mod opt-out
+		true        | ["notebook.mod.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "true")]	// Fabric mod opt-in
+		false       | ["hello.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "false")]	// None opt-out
+		true        | ["hello.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "true")]	// None opt-int
+		false        | ["hello.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "broken")]// Invalid format
+		false        | ["hello.json": "{}",
+			"META-INF/MANIFEST.MF": manifest("Something", "Hello")]			// Invalid format
+	}
+
+	def "Installer data"() {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		then:
+		isLoader == (metadata.installerData() != null)
+		where:
+		isLoader   | entries
+		true       | ["notebook.mod.json": "{}", "fabric-installer.json": "{}"] // Fabric mod, with installer data
+		false      | ["notebook.mod.json": "{}"] // Fabric mod, no installer data
+	}
+
+	def "Refmap remap type" () {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		def result = metadata.mixinRemapType()
+		then:
+		result == type
+		where:
+		type | entries
+		MIXIN       | ["hello.json": "{}"]       // None Mod jar
+		MIXIN       | ["notebook.mod.json": "{}"]  // Fabric mod without manfiest file
+		MIXIN       | ["notebook.mod.json": "{}", "META-INF/MANIFEST.MF": manifest("Fabric-Loom-Remap", "true")]              // Fabric mod without remap type entry
+		MIXIN       | ["notebook.mod.json": "{}", "META-INF/MANIFEST.MF": manifest("Fabric-Loom-Mixin-Remap-Type", "mixin")]  // Fabric mod with remap type entry "mixin"
+		STATIC      | ["notebook.mod.json": "{}", "META-INF/MANIFEST.MF": manifest("Fabric-Loom-Mixin-Remap-Type", "static")] // Fabric mod with remap type entry "static"
+	}
+
+	// Test that a mod with the same or older version of loom can be read
+	def "Valid loom version"() {
+		given:
+		def zip = createModWithRemapType(modLoomVersion, "static")
+		when:
+		def metadata = createMetadata(zip, loomVersion)
+		then:
+		metadata != null
+		where:
+		loomVersion | modLoomVersion
+		"1.4"       | "1.0.1"
+		"1.4"       | "1.0.99"
+		"1.4"       | "1.4"
+		"1.4"       | "1.4.0"
+		"1.4"       | "1.4.1"
+		"1.4"       | "1.4.99"
+		"1.4"       | "1.4.local"
+		"1.5"       | "1.4.99"
+		"2.0"       | "1.4.99"
+	}
+
+	// Test that a mod with the same or older version of loom can be read
+	def "Invalid loom version"() {
+		given:
+		def zip = createModWithRemapType(modLoomVersion, "static")
+		when:
+		def metadata = createMetadata(zip, loomVersion)
+		then:
+		def e = thrown(IllegalStateException)
+		e.message == "Mod was built with a newer version of Loom ($modLoomVersion), you are using Loom ($loomVersion)"
+		where:
+		loomVersion | modLoomVersion
+		"1.4"       | "1.5"
+		"1.4"       | "1.5.00"
+		"1.4"       | "2.0"
+		"1.4"       | "2.4"
+	}
+
+	def "Accepts all Loom versions for remap 'false'"() {
+		given:
+		def zip = createModWithRemap(modLoomVersion, false)
+		when:
+		def metadata = createMetadata(zip, loomVersion)
+		then:
+		metadata != null
+		where:
+		loomVersion | modLoomVersion
+		// Valid
+		"1.4"       | "1.0.1"
+		"1.4"       | "1.0.99"
+		"1.4"       | "1.4"
+		"1.4"       | "1.4.0"
+		"1.4"       | "1.4.1"
+		"1.4"       | "1.4.99"
+		"1.4"       | "1.4.local"
+		"1.5"       | "1.4.99"
+		"2.0"       | "1.4.99"
+		// Usually invalid
+		"1.4"       | "1.5"
+		"1.4"       | "1.5.00"
+		"1.4"       | "2.0"
+		"1.4"       | "2.4"
+	}
+
+	def "Accepts all Loom versions for remap 'true'"() {
+		given:
+		def zip = createModWithRemap(modLoomVersion, true)
+		when:
+		def metadata = createMetadata(zip, loomVersion)
+		then:
+		metadata != null
+		where:
+		loomVersion | modLoomVersion
+		// Valid
+		"1.4"       | "1.0.1"
+		"1.4"       | "1.0.99"
+		"1.4"       | "1.4"
+		"1.4"       | "1.4.0"
+		"1.4"       | "1.4.1"
+		"1.4"       | "1.4.99"
+		"1.4"       | "1.4.local"
+		"1.5"       | "1.4.99"
+		"2.0"       | "1.4.99"
+		// Usually invalid
+		"1.4"       | "1.5"
+		"1.4"       | "1.5.00"
+		"1.4"       | "2.0"
+		"1.4"       | "2.4"
+	}
+
+	def "known indy BSMs"() {
+		given:
+		def zip = createZip(entries)
+		when:
+		def metadata = createMetadata(zip)
+		then:
+		knownBSMs == metadata.knownIdyBsms()
+		where:
+		knownBSMs | entries
+		[]                    | ["notebook.mod.json": "{}"] // Default
+		["com/example/Class"] | ["META-INF/MANIFEST.MF": manifest("Fabric-Loom-Known-Indy-BSMS", "com/example/Class")] // single bsm
+		[
+			"com/example/Class",
+			"com/example/Another"
+		] | ["META-INF/MANIFEST.MF": manifest("Fabric-Loom-Known-Indy-BSMS", "com/example/Class,com/example/Another")] // two bsms
+	}
+
+	private static Path createModWithRemapType(String loomVersion, String remapType) {
+		return createZip(["notebook.mod.json": "{}", "META-INF/MANIFEST.MF": manifest(["Fabric-Loom-Version": loomVersion, "Fabric-Loom-Mixin-Remap-Type": remapType])])
+	}
+
+	private static Path createModWithRemap(String loomVersion, boolean remap) {
+		return createZip(["notebook.mod.json": "{}", "META-INF/MANIFEST.MF": manifest(["Fabric-Loom-Version": loomVersion, "Fabric-Loom-Mixin-Remap": remap ? "true" : "false"])])
+	}
+
+	private static ArtifactMetadata createMetadata(Path zip, String loomVersion = "1.4") {
+		return ArtifactMetadata.create(createArtifact(zip), loomVersion)
+	}
+
+	private static ArtifactRef createArtifact(Path zip) {
+		return new ArtifactRef.FileArtifactRef(zip, "com.bookkeepersmc", "loom-test", "1.0")
+	}
+}
